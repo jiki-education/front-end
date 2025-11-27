84f6a08fcc94dc29bfc16998ebdec05aecd82210
dem4ron <demaaron88@gmail.com>
2025-11-27 11:22:47 +0100

    Fix colour transition on page load

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark" data-theme="dark">
+    <html lang="en">
       {/*<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>*/}
       <body className={`${poppins.variable} ${sourceCodePro.variable} antialiased ui-body`}>
         <Script src="/static/theme-script.js" strategy="beforeInteractive" />
