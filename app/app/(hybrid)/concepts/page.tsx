28c2c1d2eb1b68c9654e19012dbfc98b69d9fa52
dem4ron <demaaron88@gmail.com>
2025-12-04 10:04:44 +0100

    Use dynamic isAuth in concepts page

--- app/app/(hybrid)/concepts/page.tsx
+++ app/app/(hybrid)/concepts/page.tsx
@@ -1,10 +1,15 @@
+"use client";
+
 import ConceptsListPage from "@/components/concepts/ConceptsListPage";
 import SidebarLayout from "@/components/layout/SidebarLayout";
+import { useAuthStore } from "@/lib/auth/authStore";
 
 export default function AppConceptsPage() {
+  const { isAuthenticated } = useAuthStore();
+
   return (
     <SidebarLayout activeItem="concepts">
-      <ConceptsListPage authenticated />
+      <ConceptsListPage authenticated={isAuthenticated} />
     </SidebarLayout>
   );
 }
