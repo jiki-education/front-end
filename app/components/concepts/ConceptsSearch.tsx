372c6eb4044a73a847732f8e906d23ed03821386
dem4ron <demaaron88@gmail.com>
2026-01-19 23:34:20 +0100

    Add clear-search btn design

--- app/components/concepts/ConceptsSearch.tsx
+++ app/components/concepts/ConceptsSearch.tsx
@@ -1,5 +1,6 @@
 import SearchIcon from "@static/icons/search.svg";
 import { useRef } from "react";
+import styles from "./ConceptsSearch.module.css";
 
 interface ConceptsSearchProps {
   searchQuery: string;
@@ -26,7 +27,7 @@ export default function ConceptsSearch({
   };
   return (
     <div>
-      <div className="ui-search-input">
+      <div className={`ui-search-input ${styles.searchBar} ${searchQuery ? styles.hasValue : ""}`}>
         <SearchIcon />
         <input
           ref={inputRef}
@@ -35,13 +36,16 @@ export default function ConceptsSearch({
           value={searchQuery}
           onChange={onSearchChange}
         />
-        {searchQuery && (
-          <button onClick={handleClearSearch} type="button">
-            <svg width="16" height="16" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-            </svg>
-          </button>
-        )}
+        <button
+          onClick={handleClearSearch}
+          type="button"
+          className={styles.searchClearBtn}
+          style={{ backgroundColor: "#cbd5e1" }}
+        >
+          <svg width="10" height="10" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M9 3L3 9M3 3L9 9" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
+          </svg>
+        </button>
       </div>
 
       {/* Search Results Info */}
