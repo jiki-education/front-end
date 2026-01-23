fc84bf37b9c4493fa714a5732eee98d046f5fba9
Aron Demeter <66035744+dem4ron@users.noreply.github.com>
2026-01-23 16:41:07 +0100

    Add hook to delay showing loading skeleton (#254)

--- app/components/dashboard/exercise-path/ExercisePath.tsx
+++ app/components/dashboard/exercise-path/ExercisePath.tsx
@@ -6,6 +6,7 @@ import { useLessonNavigation } from "./hooks/useLessonNavigation";
 import { useMilestoneHandler } from "./hooks/useMilestoneHandler";
 import { useLevels } from "./hooks/useLevels";
 import { useLessonCompletionAnimation } from "./hooks/useLessonCompletionAnimation";
+import { useDelayedLoading } from "@/lib/hooks/useDelayedLoading";
 import { LevelSection } from "./ui/LevelSection";
 import styles from "./ExercisePath.module.css";
 import { StartCard } from "./ui/StartCard";
@@ -13,6 +14,7 @@ import { useEffect } from "react";
 
 export default function ExercisePath() {
   const { levelSections, setLevels, levelsLoading } = useLevels();
+  const shouldShowSkeleton = useDelayedLoading(levelsLoading);
   const { handleLessonNavigation, clickedLessonSlug, setClickedLessonSlug, isPending } = useLessonNavigation();
   const { handleMilestoneClick, levelCompletionInProgress } = useMilestoneHandler(setLevels);
   const { animationState, triggerCompletionAnimation, recentlyUnlockedLessons } = useLessonCompletionAnimation();
@@ -42,7 +44,7 @@ export default function ExercisePath() {
     handleLessonNavigation(route);
   };
 
-  if (levelsLoading) {
+  if (shouldShowSkeleton) {
     return <ExercisePathSkeleton />;
   }
 
