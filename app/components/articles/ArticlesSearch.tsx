1de439039af31d05952eea96bebc491da7569406
dem4ron <demaaron88@gmail.com>
2026-01-30 16:53:20 +0100

    Add article search skinning

--- app/components/articles/ArticlesSearch.tsx
+++ app/components/articles/ArticlesSearch.tsx
@@ -1,17 +1,45 @@
 "use client";
 
+import SearchIcon from "@/icons/search.svg";
+import CrossIcon from "@/icons/cross.svg";
+import styles from "./ArticlesSearch.module.css";
+
 interface ArticlesSearchProps {
   searchQuery: string;
   onSearchChange: (query: string) => void;
+  onClearSearch: () => void;
+  searchResults: string[] | null;
 }
 
-export default function ArticlesSearch({ searchQuery, onSearchChange }: ArticlesSearchProps) {
+export default function ArticlesSearch({
+  searchQuery,
+  onSearchChange,
+  onClearSearch,
+  searchResults
+}: ArticlesSearchProps) {
+  const showNoResults = searchQuery && searchResults !== null && searchResults.length === 0;
+
   return (
-    <input
-      type="text"
-      value={searchQuery}
-      onChange={(e) => onSearchChange(e.target.value)}
-      placeholder="Search articles..."
-    />
+    <div>
+      <div className={`ui-search-input ${styles.searchBar} ${searchQuery ? styles.hasValue : ""}`}>
+        <SearchIcon />
+        <input
+          type="text"
+          placeholder="Search articles..."
+          value={searchQuery}
+          onChange={(e) => onSearchChange(e.target.value)}
+        />
+        <button onClick={onClearSearch} type="button" className={styles.searchClearBtn}>
+          <CrossIcon />
+        </button>
+      </div>
+
+      {showNoResults && (
+        <div className={styles.noResults}>
+          <p className={styles.noResultsTitle}>0 results for &ldquo;{searchQuery}&rdquo;</p>
+          <p className={styles.noResultsMessage}>Try a different search term or browse the articles.</p>
+        </div>
+      )}
+    </div>
   );
 }
